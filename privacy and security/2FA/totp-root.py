0e6146ba9c48f84f64082ca7ed02de95917302e6
BRUNO_SILVA <62109662+BrunoMeloso@users.noreply.github.com>
2024-11-30 19:20:20 -0300

    Add files via upload
    esse programa gera a senha apartir de uma chave mestra fornecida pelo serviço, sem libs externas, apartir dele você pode editar e gerar seu próprio gerador de senhas

---
+++ privacy and security/2FA/totp-root.py
@@ -0,0 +1,35 @@
+import hmac
+import hashlib
+import time
+import struct
+import base64
+from datetime import datetime
+
+# Obtém a hora atual
+hora_atual = datetime.now()
+
+
+def generate_totp(secret, time_step=30):
+    # Decodifica a chave secreta (Base32 para bytes)
+    key = base64.b32decode(secret, casefold=True)
+
+    # Calcula o timestamp atual
+    current_time = int(time.time()) // time_step
+
+    # Codifica o timestamp como bytes
+    time_bytes = struct.pack(">Q", current_time)
+
+    # Gera o HMAC usando SHA-1
+    hmac_hash = hmac.new(key, time_bytes, hashlib.sha1).digest()
+
+    # Realiza o dynamic truncation para obter um código de 6 dígitos
+    offset = hmac_hash[-1] & 0x0F
+    truncated_hash = hmac_hash[offset:offset + 4]
+    code = struct.unpack(">I", truncated_hash)[0] & 0x7FFFFFFF
+    return code % 10**6  # Retorna os 6 dígitos
+
+# Exemplo de uso
+print("digite a chave do TOTP: ")
+secret = input (" ") # Chave secreta (em Base32)
+print("Código TOTP de", hora_atual.strftime("%M:%S"), "é: ", generate_totp(secret))
+
